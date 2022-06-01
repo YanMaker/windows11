a3d06a1f50a3d317dbdea84e6cd3e93f05efb0a4
YanMaker <94195591+YanMaker@users.noreply.github.com>
2022-06-01 12:20:11 -0400

    fix another typo

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+let startbutton = document.getElementsByClassName("startbutton")[0]
+let startmenu = document.getElementsByClassName("startmenu")[0]
+startbutton.addEventListener("click", ()=>{
 
+    if(startmenu.style.bottom == "55px"){
+        startmenu.style.bottom = "-95%"
+    }
+    else{
+        startmenu.style.bottom = "55px"
+    }
+})
